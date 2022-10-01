d3c9ed40fbe8d5bb7a70d5c14446e0b88e294149
Roberto Lorenzon <roberto.lorenzon.2001@gmail.com>
2022-10-01 18:36:21 +0200

    alcune modifiche

--- lua/_catpuccin.lua
+++ lua/_catpuccin.lua
@@ -1,2 +1,12 @@
 local catppuccin = require("catppuccin")
 
+catppuccin.setup{
+    integrations = {
+        coc_nvim = true,
+        nvimtree = {
+            enable = true,
+            show_root = true,
+            transparent_panel = false
+        }
+    }
+}
